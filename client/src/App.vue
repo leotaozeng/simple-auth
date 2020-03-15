e93281614b3e9f8b247e28f72564a4e1380cdc45
Zeng Tao <qingchun1995@gmail.com>
2020-03-15 12:56:23 +0800

    Update dependencies

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,31 +7,15 @@
         <router-view />
       </transition>
     </div>
-
-    <Loader />
   </div>
 </template>
 
 <script>
-import Loader from '@/components/Loader'
 import Navbar from '@/components/Navbar'
 
 export default {
   components: {
-    Loader,
     Navbar
   }
 }
 </script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
-</style>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './modules/auth'
-import loader from './modules/loader'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth,
-    loader
+    auth
   }
 })
